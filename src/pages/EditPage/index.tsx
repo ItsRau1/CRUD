692d37b2d5d5a63fbc14dd71ecbbeb5401623678
ItsRau1 <00001085823507sp@al.educacao.sp.gov.br>
2022-12-09 15:51:44 -0300

    day updates

--- src/pages/EditPage/index.tsx
+++ src/pages/EditPage/index.tsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
-import { BoxForm, ContainerEdit, EditBox, EditButton, EditInput, FieldButton, FieldInputs, FieldTitle } from "./styles/styles";
+import { BoxForm, ContainerEdit, EditBox, EditButton, EditInput, FieldButton, FieldInputs, FieldTitle, StylesRadixUI } from "./styles/styles";
 import { NavBar } from "../../components/NavBar";
 import { MainEdit } from "./styles/styles";
 import { AuthContext } from "../../contexts/auth";
 import { ContextType } from "../../@types/types";
+import  * as AlertDialog  from "@radix-ui/react-alert-dialog";
 
 export function EditPage () {
     
@@ -49,11 +50,34 @@ export function EditPage () {
                             />
                         </FieldInputs>
                         <FieldButton>
-                            <EditButton
-                                type="submit"
-                            >
-                                Editar
-                            </EditButton>
+                            <StylesRadixUI />
+                            <AlertDialog.Root>
+                                <AlertDialog.Trigger asChild>
+                                    <EditButton
+                                    >
+                                        Editar
+                                    </EditButton>
+                                </AlertDialog.Trigger>
+                                <AlertDialog.Portal>
+                                    <AlertDialog.Overlay className="AlertDialogOverlay" />
+                                    <AlertDialog.Content className="AlertDialogContent">
+                                        <AlertDialog.Title className="AlertDialogTitle">Voce tem certeza?</AlertDialog.Title>
+                                        <AlertDialog.Description className="AlertDialogDescription">
+                                            Deseja alterar suas informacioes?
+                                        </AlertDialog.Description>
+                                        <div style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}>
+                                            <AlertDialog.Cancel asChild>
+                                                <button className="Button mauve">Cancelar</button>
+                                            </AlertDialog.Cancel>
+                                            <AlertDialog.Action asChild>
+                                                <button className="Button red" onClick={handleEdit}>
+                                                    Sim, desejo alterar
+                                                </button>
+                                            </AlertDialog.Action>   
+                                        </div>
+                                    </AlertDialog.Content>
+                                </AlertDialog.Portal>
+                            </AlertDialog.Root>
                         </FieldButton>
                     </BoxForm>
                 </EditBox>
--- src/pages/EditPage/styles/styles.ts
+++ src/pages/EditPage/styles/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 
 export const ContainerEdit = styled.div`
     display: flex;
@@ -30,6 +30,7 @@ export const EditBox = styled.div`
 
 export const FieldTitle = styled.div`
     letter-spacing: 1px;
+    color: #018CA9;
 `
 
 export const BoxForm = styled.form`
@@ -41,16 +42,163 @@ export const BoxForm = styled.form`
 export const FieldInputs = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 2rem;
+    gap: 0.5rem;
+
+    letter-spacing: 1px;
+    color: #5295AA;
 `
 export const EditInput = styled.input`
+    all: unset;
 
-`
+    padding: 15px 15px;
+    border: 1px solid #01A7C2;
+    background-color: #EAEBED;
+    font-size: 15px;
+    border-radius: 8px;
+    transition: outline 0.2s, border-color 0.2s;
+    margin: 0 0 32px 0;
+    color: #000;
+
+    &:focus{
+        outline: 1px solid #01A7C2;
+    }
+
+    &:hover{
+        border: 1px solid #006989;
+    }
+    
+    
+    `
 
 export const FieldButton = styled.div`
+    display: flex;
+    justify-content: center;
+    `
 
+    export const EditButton = styled.button`
+        all: unset;
+    
+        font-weight: 700;
+        letter-spacing: 1px;
+        color: #FFF;
+        background-color: #018CA9;
+        border-radius: 8px;
+        padding: 15px 40px;  
+        transition: filter 0.3s ;
+    
+        &:hover{
+            filter: brightness(0.7);
+        }
+    `
+// Key Frames Radix UI
+
+const contentShow = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(-50%, -48%) scale(0.96);
+  }
+  to {
+    opacity: 1;
+    transform: translate(-50%, -50%) scale(1);
+  }
 `
 
-export const EditButton = styled.button`
+const overlayShow = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+// Styles Radix UI
+
+export const StylesRadixUI = createGlobalStyle`
+button {
+  all: unset;
+}
+
+.AlertDialogOverlay {
+  background-color: hsla(0, 0%, 0%, 0.439);
+  position: fixed;
+  inset: 0;
+  animation: ${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
+}
+
+.AlertDialogContent {
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 500px;
+  max-height: 85vh;
+  padding: 25px;
+  animation: ${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
+}
+.AlertDialogContent:focus {
+  outline: none;
+}
+
+.AlertDialogTitle {
+  margin: 0;
+  color: hsl(260, 25.0%, 11.0%);
+  font-size: 17px;
+  font-weight: 500;
+}
+
+.AlertDialogDescription {
+  margin-bottom: 20px;
+  color: hsl(252, 4.0%, 44.8%);
+  font-size: 15px;
+  line-height: 1.5;
+}
+
+.Button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  padding: 0 15px;
+  font-size: 15px;
+  line-height: 1;
+  font-weight: 500;
+  height: 35px;
+}
+.Button.violet {
+  background-color: white;
+  color: #18AFC8;
+  box-shadow: 0 2px 10px hsla(0, 0%, 0%, 0.141);
+}
+.Button.violet:hover {
+  background-color: hsl(294, 5.5%, 95.3%);
+}
+.Button.violet:focus {
+  box-shadow: 0 0 0 2px black;
+}
+.Button.red {
+  background-color: hsl(360, 97.9%, 94.8%);
+  color: hsl(358, 65.0%, 48.7%);
+}
+.Button.red:hover {
+  background-color: hsl(360, 90.2%, 91.9%);
+}
+.Button.red:focus {
+  box-shadow: 0 0 0 2px hsl(359, 74.2%, 81.7%);
+}
+.Button.mauve {
+  background-color: hsl(289, 4.7%, 93.3%);
+  color: hsl(252, 4.0%, 44.8%);
+}
+.Button.mauve:hover {
+  background-color: hsl(283, 4.4%, 91.3%);
+}
+.Button.mauve:focus {
+  box-shadow: 0 0 0 2px hsl(271, 3.9%, 86.3%);
+}
 
 `
